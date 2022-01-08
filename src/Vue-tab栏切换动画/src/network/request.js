bd809d207bf26ae174ab7a64ee8069cb5475aa1a
lengyibai <1329670984@qq.com>
2022-01-08 22:01:37 +0800

    精简代码

--- src/Vue-tab栏切换动画/src/network/request.js
+++ src/Vue-tab栏切换动画/src/network/request.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 import apiConfig from '../config/api.config';
-// import { $lybPA1 } from '@/common/lyb.vue.js';
 
 //!··········配置默认参数··········!//
-// axios.defaults.baseURL = '/api';
 axios.defaults.baseURL = apiConfig.baseURL;
 axios.defaults.timeout = 30000;
 
@@ -12,19 +10,3 @@ axios.defaults.timeout = 30000;
 export function getReq(url, params, other = {}) {
   return axios.get(url, { params }, other);
 }
-
-//@········POST请求········@//
-export function postReq(url, data, other = {}) {
-  return axios.post(url, data, other);
-}
-
-//!··········拦截器··········!//
-//@········请求拦截器········@//
-axios.interceptors.request.use(config => {
-  // config.headers.token = $lybPA1.get('token');
-  return config;
-});
-//@········响应拦截器········@//
-axios.interceptors.response.use(res => {
-  return res;
-});
