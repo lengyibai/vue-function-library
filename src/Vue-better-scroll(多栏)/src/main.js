d8e29094537c1ef41395533182003f137e42d73d
lengyibai <1329670984@qq.com>
2022-01-09 03:27:46 +0800

    文件夹改名

---
+++ src/Vue-better-scroll(多栏)/src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+import App from './App.vue';
+import less from 'less';
+Vue.use(less);
+import './assets/css/default.css';
+
+Vue.config.productionTip = false;
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app');
