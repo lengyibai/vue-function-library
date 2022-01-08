29e7d8c6eab88c0225da5ebc15e00eecf40c5834
lengyibai <1329670984@qq.com>
2022-01-09 02:56:20 +0800

    删除滚动组件引用

--- src/Vue-上拉加载下拉刷新(多栏)/src/components/common/index.js
+++ src/Vue-上拉加载下拉刷新(多栏)/src/components/common/index.js
@@ -1,10 +1,8 @@
 //返回顶部
 import lybPdrPul from './lyb-pdr-pul/lyb-pdr-pul.vue';
-//滚动组件精简版
-import scroll from './scroll/scroll.vue';
 
 //使用组件
-const components = [lybPdrPul,scroll];
+const components = [lybPdrPul];
 const lybUI = {
   //只会执行install
   install(Vue) {
