0a74880035d62a8e77e828abe4c3841c22f8efe9
lengyibai <1329670984@qq.com>
2022-01-09 03:02:27 +0800

    增加解决跨域的注释

--- src/Vue-上拉加载下拉刷新(多栏)/vue.config.js
+++ src/Vue-上拉加载下拉刷新(多栏)/vue.config.js
@@ -6,7 +6,11 @@ module.exports = {
     proxy: {
       '/api': {
         target: 'https://api.web.360kan.com',
+        //设置是否跨域，开启代理：在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样客户端端和服务端进行数据的交互就不会有跨域问题
         changeOrigin: true,
+        // 是否启用websockets
+        ws: true,
+        //访问路径重写
         pathRewrite: {
           '^/api': '',
         },
