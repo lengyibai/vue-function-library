ebf75def081087ed3695c072d57db24a0dfeb2ad
lengyibai <1329670984@qq.com>
2022-01-09 23:11:02 +0800

    完美封装scroll组件

--- src/Vue-better-scroll(多栏)/src/views/home/MultiColumn.vue
+++ src/Vue-better-scroll(多栏)/src/views/home/MultiColumn.vue
@@ -13,96 +13,33 @@
       </div>
     </div>
     <div class="slide" ref="box">
-      <!-- 第一个 -->
-      <div class="lyb">
-        <div class="box" ref="scroll1">
-          <div class="better-content">
-            <p v-for="(item, index) in 100" :key="index">{{ item }}</p>
-          </div>
-        </div>
-        <back-top :show="backTopShow[0]" :father="scroll.scroll1" />
-      </div>
-      <!-- 第二个 -->
-      <div class="lyb">
-        <div class="box" ref="scroll2">
-          <div class="better-content">
-            <p v-for="(item, index) in 100" :key="index">{{ item }}</p>
-          </div>
-        </div>
-        <back-top :show="backTopShow[1]" :father="scroll.scroll2" />
-      </div>
-      <!-- 第三个 -->
-      <div class="lyb">
-        <div class="box" ref="scroll3">
-          <div class="better-content">
-            <p v-for="(item, index) in 100" :key="index">{{ item }}</p>
-          </div>
-        </div>
-        <back-top :show="backTopShow[2]" :father="scroll.scroll3" />
-      </div>
+      <scroll>
+        <p v-for="(item, index) in 100" :key="index">{{ item }}</p>
+      </scroll>
+      <scroll>
+        <p v-for="(item, index) in 100" :key="index">{{ item }}</p>
+      </scroll>
+      <scroll>
+        <p v-for="(item, index) in 100" :key="index">{{ item }}</p>
+      </scroll>
     </div>
   </div>
 </template>
 <script>
-  //!··········公共组件··········!//
-  import BScroll from '@better-scroll/core';
-  // import ObserveImage from '@better-scroll/observe-image';
-  // import ObserveDOM from '@better-scroll/observe-dom';
-  // BScroll.use(ObserveDOM);
-  // BScroll.use(ObserveImage);
-  import backTop from './back-top/back-top.vue';
+  import scroll from './scroll';
   export default {
-    props: {},
     data() {
       return {
         tabName: ['one', 'two', 'three'],
         currentIndex: 0,
-        top: '0px',
-        bottom: '0px',
-        scroll: { scroll1: null, scroll2: null, scroll3: null },
-        backTopShow: [false, false, false],
       };
     },
-    mounted() {
-      let attr = {
-        // observeImage: true,
-        // observeDOM: true,
-        click: true,
-        useTransition: false,
-        // 监听滚动
-        probeType: 3,
-      };
-      this.$nextTick(() => {
-        Object.keys(this.scroll).forEach(item => {
-          this.scroll[item] = new BScroll(this.$refs[item], attr);
-        });
-        //监听滚动位置
-        Object.keys(this.scroll).forEach((item, index) => {
-          this.scroll[item].on('scroll', position => {
-            this.$set(
-              this.backTopShow,
-              index,
-              -position.y > this.$refs[item].clientHeight,
-            );
-          });
-        });
-      });
-    },
-    components: { backTop },
-    ////···············methods···············////
+    components: { scroll },
     methods: {
       //@tab栏点击
       tabClick(index) {
         this.currentIndex = index;
         this.$refs.box.style.transform = `translateX(-${index * 100}%)`;
-      },
-
-      scrollTo(x, y, time = 300) {
-        this.scroll1.scrollTo(x, y, time);
-      },
-
-      refresh() {
-        this.scroll1 && this.scroll1.refresh();
       },
     },
   };
@@ -138,23 +75,6 @@
       width: 100%;
       height: calc(100vh - 50px);
       transition: all 0.25s;
-      .lyb {
-        position: relative;
-        display: flex;
-        width: 100%;
-        flex-shrink: 0;
-        .box {
-          width: 100%;
-          overflow: hidden;
-          .better-content {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            font-size: 25px;
-            color: #fff;
-          }
-        }
-      }
     }
   }
   .active {
